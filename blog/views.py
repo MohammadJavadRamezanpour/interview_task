78e2b2e93f19fbf980d422defd39d08bd3bec312
Mohammad Javad <asfsdfsd@dsdf.com>
2022-04-25 07:38:51 +0430

    docstring added to views

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,19 @@ from django.db.models import Avg
 
 # Create your views here.
 class PostView(ListCreateAPIView):
+    """
+    this route lists all posts for anyone
+    fields:
+        id: posts id
+        title: posts title
+        voted_users_count: number of users who had voted for this post
+        rating: the rating of this post, the average of all votes
+        your_rating: the score you gave to this post, available only if you voted
+    to create a post you have to be admin
+    in order to vote, head over to /rate
+    to create a post, post 'title' and 'body' to this route as admin
+    """
+
     # we use prefetch_related for optimization
     # cause we assume we have a huge number of reviews for each post
     # prefetch_related does a separate lookup for each relationship, and performs the joining in python
@@ -23,6 +36,13 @@ class PostView(ListCreateAPIView):
 
 
 class RatingView(CreateAPIView):
+    """
+    this route lets you vote!
+    only authenticated users can make post request to this
+    to vote send these:
+    post: post id as an integer
+    score, an integer number between 0 to 5
+    """
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     permission_classes = [IsAuthenticated] # only authenticated users can vote
