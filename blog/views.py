4d034313e0d2da497633b073a7d9aed5a4ea6c4d
Mohammad Javad <asfsdfsd@dsdf.com>
2022-04-25 07:28:41 +0430

    get_queryset method changed to queryset attribute in the PostView

--- blog/views.py
+++ blog/views.py
@@ -8,19 +8,16 @@ from django.db.models import Avg
 
 # Create your views here.
 class PostView(ListCreateAPIView):
+    # we use prefetch_related for optimization
+    # cause we assume we have a huge number of reviews for each post
+    # prefetch_related does a separate lookup for each relationship, and performs the joining in python
+    # we annotate the _rating to the queryset and send it from model to the serializer
+    # i think this is much more faster and optimized, because you said we have a huge number of reviews
+    queryset = Post.objects.prefetch_related('ratings').all().annotate(_rating=Avg('ratings__score'))
     serializer_class = PostSerializer
     permission_classes = [IsAdminOrReadOnly] # admins can create post, users can just watch and rate
-
-    def get_queryset(self):
-        """
-          we use prefetch_related for optimization
-          cause we assume we have a huge number of reviews for each post
-          prefetch_related does a separate lookup for each relationship, and performs the joining in python
-          we annotate the _rating to the queryset and send it from model to the serializer
-          i think this is much more faster and optimized, because you said we have a huge number of reviews
-        """
-        return Post.objects.prefetch_related('ratings').all().annotate(_rating=Avg('ratings__score'))
     
+
     def get_serializer_context(self):
         return {'request': self.request}
 
